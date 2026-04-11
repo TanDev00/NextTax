b29da78d0e4bf84ab1159730beaaa452eba918c6
Tankmt113 <dotrongtan113@gmail.com>
2026-04-11 15:04:10 +0700

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,18 @@ import icon from 'astro-icon';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [icon()],
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
